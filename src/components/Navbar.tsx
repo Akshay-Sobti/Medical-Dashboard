85dfc717f7f95dd39227f6525aeb20cf4d9c2d94
Akshay-Sobti <akshaysobti2002@gmail.com>
2025-02-08 11:04:55 +0530

    Update Navbar.tsx

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,1 +1,20 @@
+import { Box, Typography } from "@mui/material";
+
+const Navbar = () => {
+  return (
+    <Box
+      sx={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+        p: 2,
+        bgcolor: "#F5F8FC",
+      }}
+    >
+      <Typography variant="h6">DASHBOARD</Typography>
+    </Box>
+  );
+};
+
+export default Navbar;
 
